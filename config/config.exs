4a8543f3812ef84d18c616afb865d2c1c69a1ca4
jgittler <jason.gittler@gmail.com>
2018-02-03 13:50:53 -0800

    remove configs

--- config/config.exs
+++
@@ -1,7 +0,0 @@
-use Mix.Config
-
-config :procore,
-  http_client: Procore.HttpClient,
-  oauth: Procore.HttpClient.OAuth
-
-import_config "#{Mix.env()}.exs"
--- config/test.exs
+++
@@ -1,6 +0,0 @@
-use Mix.Config
-
-config :procore,
-  http_client: HttpClient.MockClient,
-  oauth: HttpClient.MockOAuth,
-  host: "https://test.procore.com"
