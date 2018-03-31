9fd0f61cdfedd29ec598543e3ef4c55ab4e3d253
jgittler <jason.gittler@gmail.com>
2018-03-31 11:11:03 -0700

    list and create for woc line items

---
+++ test/resources/work_order_contract_line_items_test.exs
@@ -0,0 +1,19 @@
+defmodule Procore.Resources.WorkOrderContractLineItemsTest do
+  use ExUnit.Case
+  alias Procore.ResponseResult
+  alias Procore.Resources.WorkOrderContractLineItems
+
+  test "list/1" do
+    params = %{"project_id" => 1, "work_order_contract_id" => 1}
+
+    assert %ResponseResult{reply: :ok, status_code: 200, parsed_body: []} =
+             WorkOrderContractLineItems.list(params)
+  end
+
+  test "create/1" do
+    params = %{"project_id" => 1, "work_order_contract_id" => 1, "line_item" => %{}}
+
+    assert %ResponseResult{reply: :ok, status_code: 201, parsed_body: %{}} =
+             WorkOrderContractLineItems.create(params)
+  end
+end
