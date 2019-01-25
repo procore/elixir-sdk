94401a9a3019fddbe1113f5dc5166a97f7ea2f47
jgittler <jason.gittler@gmail.com>
2019-01-25 12:09:13 -0800

    fix observation item response logs specs; remove unavailable find request

--- lib/procore/resources/observation_item_response_logs.ex
+++ lib/procore/resources/observation_item_response_logs.ex
@@ -10,8 +10,10 @@ defmodule Procore.Resources.ObservationItemResponseLogs do
   @doc """
   Lists all ObservationItemResponseLogs in a ObservationItem.
   """
-  @spec list(Tesla.Client.t(), %{(company_id :: String.t()) => pos_integer}) ::
-          %ResponseResult{} | %ErrorResult{}
+  @spec list(Tesla.Client.t(), %{
+          (project_id :: String.t()) => pos_integer,
+          (observation_item_id :: String.t()) => pos_integer
+        }) :: %ResponseResult{} | %ErrorResult{}
   def list(client, %{"project_id" => project_id, "observation_item_id" => item_id}) do
     %Request{}
     |> Request.insert_request_type(:get)
@@ -21,26 +23,13 @@ defmodule Procore.Resources.ObservationItemResponseLogs do
   end
 
   @doc """
-  Gets a single ObservationItemResponseLog.
-  """
-  @spec find(Tesla.Client.t(), %{
-          (company_id :: String.t()) => pos_integer,
-          (observationitemresponse_log_id :: String.t()) => pos_integer
-        }) :: %ResponseResult{} | %ErrorResult{}
-  def find(client, %{"project_id" => project_id, "observation_item_id" => item_id, "id" => id}) do
-    %Request{}
-    |> Request.insert_request_type(:get)
-    |> Request.insert_endpoint("/vapid/observations/items/#{item_id}/response_logs/#{id}")
-    |> Request.insert_query_params(%{"project_id" => project_id})
-    |> Procore.send_request(client)
-  end
-
-  @doc """
   Creates an ObservationItemResponseLog.
   """
   @spec create(Tesla.Client.t(), %{
-          (company_id :: String.t()) => pos_integer,
-          (observationitemresponse_log :: String.t()) => map
+          (project_id :: String.t()) => pos_integer,
+          (response_log :: String.t()) => map,
+          (status :: String.t()) => String.t(),
+          (attachments :: String.t()) => list
         }) :: %ResponseResult{} | %ErrorResult{}
   def create(client, %{
         "project_id" => project_id,
--- test/resources/observation_item_response_logs_test.exs
+++ test/resources/observation_item_response_logs_test.exs
@@ -11,14 +11,6 @@ defmodule Procore.Resources.ObservationItemResponseLogsTest do
              ObservationItemResponseLogs.list(client, params)
   end
 
-  test "find/1" do
-    client = Procore.client()
-    params = %{"project_id" => 1, "observation_item_id" => 1, "id" => 1}
-
-    assert %ResponseResult{reply: :ok, status_code: 200, parsed_body: %{}} =
-             ObservationItemResponseLogs.find(client, params)
-  end
-
   test "create/1" do
     client = Procore.client()
 
