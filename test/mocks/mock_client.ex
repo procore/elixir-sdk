241686c15aad223feac5a5ad9f73fe946666230f
Paul Hoffer <git@paulhoffer.com>
2019-05-04 21:57:41 -0700

    add project line item types

--- test/mocks/mock_client.ex
+++ test/mocks/mock_client.ex
@@ -167,6 +167,10 @@ defmodule HttpClient.MockClient do
     %ResponseResult{status_code: 200, parsed_body: [], reply: :ok}
   end
 
+  def get(_, "/vapid/line_item_types/1", _) do
+    %ResponseResult{status_code: 200, parsed_body: [], reply: :ok}
+  end
+
   def get(_, "/vapid/work_order_contracts", _) do
     %ResponseResult{status_code: 200, parsed_body: [], reply: :ok}
   end
@@ -293,6 +297,10 @@ defmodule HttpClient.MockClient do
     %ResponseResult{status_code: 201, parsed_body: %{}, reply: :ok}
   end
 
+  def post(_, "/vapid/line_item_types", _, _) do
+    %ResponseResult{status_code: 201, parsed_body: %{}, reply: :ok}
+  end
+
   def post(_, "/vapid/images", _, _) do
     %ResponseResult{status_code: 201, parsed_body: %{}, reply: :ok}
   end
@@ -416,6 +424,10 @@ defmodule HttpClient.MockClient do
   end
 
   def patch(_, "/vapid/budget_line_items/1", _) do
+    %ResponseResult{status_code: 200, parsed_body: %{}, reply: :ok}
+  end
+
+  def patch(_, "/vapid/line_item_typess/1", _) do
     %ResponseResult{status_code: 200, parsed_body: %{}, reply: :ok}
   end
 
